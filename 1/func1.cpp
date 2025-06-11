311fec29211d6159255cafcc71279bdaac35bce9
gaokunwang <gaokun.wang@eintik.com>
2025-06-11 10:54:44 +0800

    init proj

---
+++ 1/func1.cpp
@@ -0,0 +1,14 @@
+﻿#include "func1.h"
+#include "./ui_func1.h"
+
+Func1::Func1(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Func1)
+{
+    ui->setupUi(this);
+}
+
+Func1::~Func1()
+{
+    delete ui;
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include "func1.h"
+
+#include <QApplication>
+
+#define test1
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+#ifdef test1
+    /// func1,
+    Func1 w;
+    w.show();
+#endif
+    return a.exec();
+}
